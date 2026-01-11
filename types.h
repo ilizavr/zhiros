0b7bf93ddc107857f0b887f4b5e0b28ae6c9a8a5
ilizavr <84659093+ilizavr@users.noreply.github.com>
2026-01-11 20:20:28 +0300

    Add files via upload

---
+++ types.h
@@ -0,0 +1,9 @@
+//type defines
+typedef unsigned char U8;
+typedef unsigned short U16;
+typedef unsigned int U32;
+typedef unsigned long long U64;
+typedef void* PTR;
+typedef U64 size_t;
+
+struct vec2U8{ U8 x; U8 y;};
