82e95f12abf0d36d0dee51c4e05b6d68c8c2a1e4
дилер <asdasd1czx1@gmail.com>
2026-06-25 00:10:02 +0500

    e1000.h
    permission true for read permission to read offsets at the base address(mmio for e1000)

--- e1000.h
+++ e1000.h
@@ -1,1 +1,15 @@
 
+void e1000_read(u32 BUSMASTER, u32 MEMORYSPACE){             //permission read base address true
+	u32 pci_config_address = (u32)((busG << 16) | (slotG << 11) | (0 << 8) | (0x04 & 0xfc) | ((u32)0x80000000));
+	u32 addressRequest = pci_config_address;
+	u32 value;
+	outl(0xCF8, addressRequest);
+	print("\n"); print("addressRequest "); print_hex(addressRequest); print("\n");
+	value = inl(0xCFC);
+	print("value not changed: ");print_hex(value); print("\n");
+	
+	value |= (BUSMASTER << 2);
+	value |= (MEMORYSPACE << 1);
+	print("\n"); print("changed: "); print_hex(value); print("\n");
+	outl(0xCFC, value);
+}
